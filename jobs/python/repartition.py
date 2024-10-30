86a5919ca49d88972f9aa7d38895a393022a1153
Kamil Dereń <kamil.deren1@gmail.com>
2024-10-30 17:09:46 +0100

    Added pipeline diagram Fixed repartitioning (fully working) Renaming in pipeline

--- jobs/python/repartition.py
+++ jobs/python/repartition.py
@@ -1,18 +1,26 @@
 from pyspark.sql import SparkSession
+import os
+import shutil
 
 spark = SparkSession.builder.appName("PythonRepartition").getOrCreate()
 
 readpath = "/data/source/data1.csv"
-savepath = "/data/partitions/"
+temp_savepath = "/data/temp_partitions/"
+final_savepath = "/data/partitions/"
 
-print(f"Attempting to read file from: {readpath}")
 df = spark.read.csv(readpath, header=True, inferSchema=True)
-print(f"Success")
-
-df.show(5)
 
 df_partitioned = df.repartition(10)
-df_partitioned.write.mode("overwrite").csv(savepath, header=True)
+
+df_partitioned.write.mode("overwrite").csv(temp_savepath, header=True)
+
+# Rename files in the temporary directory
+for i, filename in enumerate(os.listdir(temp_savepath)):
+    if filename.endswith('.csv'):
+        new_name = f"milan_mobile_part{i-10}.csv"
+        os.rename(os.path.join(temp_savepath, filename), os.path.join(final_savepath, new_name))
+
+# Clean up the temporary directory
+shutil.rmtree(temp_savepath)
 
 spark.stop()
-
