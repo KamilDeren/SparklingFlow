a47ed37d0680efdc183d6ce15a26c5964fc4572e
Kamil Dereń <kamil.deren1@gmail.com>
2024-11-06 09:58:31 +0100

    Parametrized partitioning

--- jobs/python/repartition.py
+++ jobs/python/repartition.py
@@ -1,16 +1,20 @@
 from pyspark.sql import SparkSession
 import os
 import shutil
+import sys
 
 spark = SparkSession.builder.appName("PythonRepartition").getOrCreate()
 
-readpath = "/data/source/data1.csv"
-temp_savepath = "/data/temp_partitions/"
-final_savepath = "/data/partitions/"
+#Parametryzacja
 
-df = spark.read.csv(readpath, header=True, inferSchema=True)
+input_path = sys.argv[sys.argv.index('--input') + 1]
+temp_savepath = sys.argv[sys.argv.index('--temp') + 1]
+output_path = sys.argv[sys.argv.index('--output') + 1]
+partition_num = sys.argv[sys.argv.index('--partition') + 1]
 
-df_partitioned = df.repartition(10)
+df = spark.read.csv(input_path, header=True, inferSchema=True)
+
+df_partitioned = df.repartition(partition_num)
 
 df_partitioned.write.mode("overwrite").csv(temp_savepath, header=True)
 
@@ -18,7 +22,7 @@ df_partitioned.write.mode("overwrite").csv(temp_savepath, header=True)
 for i, filename in enumerate(os.listdir(temp_savepath)):
     if filename.endswith('.csv'):
         new_name = f"milan_mobile_part{i-10}.csv"
-        os.rename(os.path.join(temp_savepath, filename), os.path.join(final_savepath, new_name))
+        os.rename(os.path.join(temp_savepath, filename), os.path.join(output_path, new_name))
 
 # Clean up the temporary directory
 shutil.rmtree(temp_savepath)
