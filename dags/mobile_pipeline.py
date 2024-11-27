caca7933819c496288d2968f4c3d05908fb66908
Kamil Dereń <91948022+KamilDeren@users.noreply.github.com>
2024-11-27 10:24:13 +0100

    Update mobile_pipeline.py
    Updated pipeline order

--- dags/mobile_pipeline.py
+++ dags/mobile_pipeline.py
@@ -18,43 +18,42 @@ start = PythonOperator(
     dag=dag
 )
 
-# health_check = SparkSubmitOperator(
-#     task_id="health_check",
-#     conn_id="spark-conn",
-#     application="jobs/python/wordcountjob.py",
-#     dag=dag
-# )
+health_check = SparkSubmitOperator(
+    task_id="health_check",
+    conn_id="spark-conn",
+    application="jobs/python/wordcountjob.py",
+    dag=dag
+)
 
-# repartition = SparkSubmitOperator(
-#     task_id="repartition",
-#     conn_id="spark-conn",
-#     application="jobs/python/repartition.py",
-#     application_args=["--input", "/data/source/data1.csv",
-#                       "--temp", "/data/temp_partitions/",
-#                       "--output", "/data/bronze/partitions/"],
-#     dag=dag
-# )
+repartition = SparkSubmitOperator(
+    task_id="repartition",
+    conn_id="spark-conn",
+    application="jobs/python/repartition.py",
+    application_args=["--input", "/data/source/data1.csv",
+                      "--temp", "/data/temp_partitions/",
+                      "--output", "/data/bronze/partitions/"],
+    dag=dag
+)
 
-# faker_data = SparkSubmitOperator(
-#     task_id="faker_data",
-#     conn_id="spark-conn",
-#     application="jobs/python/generate_fake_data.py",
-#     dag=dag
-# )
+faker_data = SparkSubmitOperator(
+    task_id="faker_data",
+    conn_id="spark-conn",
+    application="jobs/python/generate_fake_data.py",
+    dag=dag
+)
 
-# internet_usage = SparkSubmitOperator(
-#     task_id="internet_usage",
-#     conn_id="spark-conn",
-#     application="jobs/python/internet_usage.py",
-#     dag=dag
-# )
-#
-# mobile_usage = SparkSubmitOperator(
-#     task_id="mobile_usage",
-#     conn_id="spark-conn",
-#     application="jobs/python/mobile_usage.py",
-#     dag=dag
-# )
+internet_usage = SparkSubmitOperator(
+    task_id="internet_usage",
+    conn_id="spark-conn",
+    application="jobs/python/internet_usage.py",
+    dag=dag
+)
+mobile_usage = SparkSubmitOperator(
+    task_id="mobile_usage",
+    conn_id="spark-conn",
+    application="jobs/python/mobile_usage.py",
+    dag=dag
+)
 
 tourist_country = SparkSubmitOperator(
     task_id="tourist_country",
@@ -69,5 +68,4 @@ end = PythonOperator(
     dag=dag
 )
 
-# start >> health_check >> repartition >> faker_data >> internet_usage >> mobile_usage >> end
-start >> tourist_country >> end
+start >> health_check >> repartition >> faker_data >> [tourist_country, internet_usage, mobile_usage] >> end
